0ee599442b013c4efeff8f1d5b45b4d3fd123089
Melania <kkj01331@gmail.com>
2020-12-02 13:35:11 +0900

    Create 해시_전화번호목록.java

---
+++ Programmers/해시_전화번호목록.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+class Solution {
+        public boolean solution(String[] phone_book) {
+            
+        boolean answer = true;
+        HashMap<String, Integer> checkMap = new HashMap<String, Integer>();
+        for(String value : phone_book) {
+            for(int i = 1; i < value.length(); i++) {
+            	checkMap.put(value.substring(0, i), 1);
+            }
+        }
+            
+        for(String value : phone_book) {
+            if(checkMap.containsKey(value)) {
+                answer = false;
+                break;
+            } else {
+            	checkMap.put(value, 1);
+            }
+        }
+            
+        return answer;
+    }
+}
