7b06be3691d7f028d057e4321f08abc6cf85d594
Melania <kkj01331@gmail.com>
2020-12-02 20:45:11 +0900

    Update 12.02_해시_완주하지못한선수.java

--- Programmers/12.02_해시_완주하지못한선수.java
+++ Programmers/12.02_해시_완주하지못한선수.java
@@ -6,9 +6,8 @@ class Solution {
         
         HashMap<String, Integer> checkMap = new HashMap<>();
         
+        //getOrDefault 찾는 키가 존재한다면 찾는 키의 값을 반환하고 없다면 기본 값을 반환
         for(String value: participant)
-            
-            //getOrDefault 찾는 키가 존재한다면 찾는 키의 값을 반환하고 없다면 기본 값을 반환
         	checkMap.put(value, checkMap.getOrDefault(value,0)+1);
         
         for(String value: completion)
